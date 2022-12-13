6186cc6fd94702affbe45a4e77f007c6bc83bdda
DayoTitiloye-code <DayoTitiloye@outlook.com>
2022-12-13 17:11:27 +0000

    added error handling

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask import render_template
+from werkzeug import exceptions
 
 app = Flask(__name__)
 CORS(app)
@@ -8,6 +9,14 @@ CORS(app)
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.errorhandler(exceptions.NotFound)
+def handle_404(err):
+    return jsonify({"message": f"Sorry... {err}"}), 404
+
+@app.errorhandler(exceptions.InternalServerError)
+def handle_500(err):
+    return jsonify({"message": f"It's not you, it's us"}), 500
 
 if __name__ == '__main__':
     app.run()
